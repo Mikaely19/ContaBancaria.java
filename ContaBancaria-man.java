3d49645423d45dd7b227ebc1d849d2f3221ad31e
Mikaely19 <125111409306@ulife.com.br>
2025-05-14 23:56:34 -0300

    Update ContaBancaria-man.java

--- ContaBancaria-man.java
+++ ContaBancaria-man.java
@@ -1,23 +1,21 @@
     public static void main(String[] args) throws Exception {
-        // Instanciando duas constas bancárias 
+        // Contas bancaária
 
         ContaBancaria conta1 = new ContaBancaria(101, "Karine", 200.0);
         ContaBancaria conta1 = new ContaBancaria(101, "Shopia", 500.0);
 
-        //Operações na conta 1
+        //Conta 1
         System.out.println("Saldo inicial da conta 1: R$ %.2f\n", , conta1.getSaldo());
-        conta1.depositar(200.0);~
-        System.out.println("Saldo após depósito de R$ 200,00: R$ %.2f\n", , conta1.getSaldo());
+        conta1.depositar(200.0);
+        System.out.println("Saldo do depósito de R$ 200,00: R$ %.2f\n", , conta1.getSaldo());
         boolean saquel1 = contal.sacar(300.0);
         System.out.printf("Saque de R$ 300,00 %s. Saldo atual : R$ %.2f/n");
                            saque1 ? "realizado com sucesso" : "falhou", conta1.gestSaldo();
 
-    System.out.println{};
-
-    // Operações na conta 2
+    // Conta 2
     System.out.printf("Saldo inicial da conta 2: R$ %.2f\n", conta2.getSaldo());
     conta2.depositar(150,0);
     System.out.printf("Saldo após depósito de R$ 50,00: R$ %2fz\n", conta2.getSaldo());
     boolean saque2 = conta2.sacar(50.0);
-    System.out.printf("Saque de R$ 500,00 %s. Saldo atual: R$ %\n"
+    System.out.printf("Saque de R$ 50,00 %s. Saldo atual: R$ %\n"
                       saque2 ? "realizado com sucesso": "falhou", conta2getSaldo()) 
